13f8bbd8ef0fcd26a5853500fa54fbd9cd69d32f
Versilis Tyson <versilistyson@gmail.com>
2022-05-17 17:57:32 -0500

    feat: add middleware logic

---
+++ pkg/middleware/doc.go
@@ -0,0 +1,4 @@
+// Package middleware contains re-usable middleware functions. Middleware
+// functions in this package follow standard http.HandlerFunc signature and
+// hence are compatible with all standard http library functions.
+package middleware
---
+++ pkg/middleware/utils.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"go-starter/pkg/logger"
+	"net/http"
+)
+
+func requestInfo(req *http.Request) map[string]interface{} {
+	return map[string]interface{}{
+		"path":   req.URL.Path,
+		"query":  req.URL.RawQuery,
+		"method": req.Method,
+		"client": req.RemoteAddr,
+	}
+}
+
+func wrap(wr http.ResponseWriter, logger logger.Logger) *wrappedWriter {
+	return &wrappedWriter{
+		ResponseWriter: wr,
+		Logger:         logger,
+		wroteStatus:    http.StatusOK,
+	}
+}
+
+type wrappedWriter struct {
+	http.ResponseWriter
+	logger.Logger
+
+	wroteStatus int
+}
+
+func (wr *wrappedWriter) WriteHeader(statusCode int) {
+	wr.wroteStatus = statusCode
+	wr.ResponseWriter.WriteHeader(statusCode)
+}
